e63df128916b2c4451891d880b79270c18466b74
Sreekanth Gampa (RBEI/BST1) <reg3kor@bosch.com>
2019-12-10 12:17:58 +0530

    initial commit

---
+++ databinding1.js
@@ -0,0 +1,14 @@
+<!DOCTYPE html>
+<html>
+<hed>
+<p1>Header is here</p1></hed>
+
+<body ng-app>
+<scriptsrc=angular.min.js>   </script>
+
+Name: <input type="text" ng-model="propetyName"/> {{propetyName}}
+
+</body>
+
+
+</html>
